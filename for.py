fb9d827a20957993efe5b1aa8aca6f59e91b1124
SergioLejarde <80810089+SergioLejarde@users.noreply.github.com>
2021-03-24 22:03:38 -0500

    esta es la tarea para hoy 24 de marzo

--- for.py
+++ for.py
@@ -1,2 +1,34 @@
-for x in range(20,41,2):#
-  print(x)
+#for i in range(9):#haga el bucle hasta nueve, la variable por defecto empieza en cero
+   # print(i)#luego va de cero hasta nueve exclusivo, es decir sin llegar al nueve
+#for rango in range(3,13,): # hace el buble desde 3 hasta 12
+   # print(rango)
+
+#for x in range(3,13,2):#el bucle va de 2 en 2 hasta llegar al 13 exclusivo, osea 12
+#       print(x)
+
+#for x in range(11,4):#el programa no se ejecuta ya que del once no se puede ir hasta el 4. falta otro parametro
+#    print(x)
+
+#for x in range(11,4,-1):#ahora si se puede ir de 11 a 4 de manera regresiva de a una unidad. el 11 es inclusivo por estar de primero y luego va hasta el 4 pero sin llegar a el porque es exclusivo
+  #  print(x) #va desde el 11 hasta el 4 exclusivo de manera regresiva
+
+#for x in range(3,-1,-1):#va desde el 3 hasta -1 en cuanto regresiva de a una unidad
+   # print(x)
+
+#for i in {0,1,2,3,4}:# creo que es el tema de listas
+       #print("hola")
+
+#for x in range(10,0,-1):
+          # print(x)
+           #print("año nuevo")#se imprime tambien este mensaje en cada iteracion
+
+#for n in range(1,13):
+  #  print(n)
+#for i in [0, 1, 2]:
+   # for j in [0, 1]:
+       # print(f"i vale {i} y j vale {j}")
+
+for i in [1, 2, 3]:
+    for j in [1, 2]:
+        print(f"i vale {i} y j vale {j}")
+        
